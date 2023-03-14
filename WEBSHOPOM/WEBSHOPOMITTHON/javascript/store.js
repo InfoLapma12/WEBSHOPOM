cb8e273aed9de8efa50dcc651177b212f3b7f8e4
InfoLapma12 <124691696+InfoLapma12@users.noreply.github.com>
2023-03-14 19:48:47 +0100

    Add files via upload

---
+++ WEBSHOPOM/WEBSHOPOMITTHON/javascript/store.js
@@ -0,0 +1,41 @@
+const cartList = document.getElementById("cart-list");
+const totalPrice = document.getElementById("total-price");
+const clearCart = document.getElementById("clear-cart");
+
+let cartItems = [];
+
+function addToCart(name, price, image) {
+  const item = { name, price, image };
+  cartItems.push(item);
+  renderCart();
+  localStorag.setItem("Termék neve:",name);
+  localStorag.setItem("Termék ára:",price);
+  localStorag.setItem("Termék képe:",image);
+}
+
+function renderCart() {
+  cartList.innerHTML = "";
+  let total = 0;
+  cartItems.forEach(item => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = item.image;
+    li.appendChild(img);
+    const spanName = document.createElement("span");
+    spanName.innerHTML = item.name;
+    li.appendChild(spanName);
+    const spanPrice = document.createElement("span");
+    spanPrice.innerHTML = item.price + " Ft";
+    li.appendChild(spanPrice);
+    cartList.appendChild(li);
+    total += item.price;
+  });
+  totalPrice.innerHTML = "Összesen: " + total + " Ft";
+}
+
+function clearCartItems() {
+  cartItems = [];
+  renderCart();
+}
+
+clearCart.addEventListener("click", clearCartItems);
